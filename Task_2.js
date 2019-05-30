84a32f5dc9fb7ccd2c5527bf919c1888fb4e93e8
ivan-ver <ivan_ver89@mail.ru>
2019-05-30 06:48:38 +0400

    Исправление

--- Task_2.js
+++ Task_2.js
@@ -1,38 +1,38 @@
 let m = [[1,1],[1,-1],[-1,1],[-1,-1]];
-
 let letters =['A','B','C','D','E','F','G','H'];
 
 ok_button.onclick = function () {
     let position = document.getElementById("position").value;
 
+    if (position.length !== 2 || isNaN(Number(position.split('')[1]))){
+        alert('Неверный формат ввода!!!');
+        return;
+    }
+
     let x = getLetterPosition(position.split('')[0]);
     let y = Number(position.split('')[1]);
 
 
-    if (position.length !== 2 || isNaN(x) || typeof position.split('')[1] !== 'string'){
-        alert('Неверный формат ввода!!!');
-        return;
-    }
-    if (x > 8 || y > 8) {
+
+    if (x > 8 || y > 8 || x < 1 || y < 1) {
         alert('Вы вышли за пределы шахматной доски!!!');
         return;
     }
-
     let coordinates = [];
 
     for (let i = 0; i < 4; i++){
         let rx = x + 2*m[i][0]-1;
         let ry = y + m[i][1];
-        if ( rx > 0 && ry > 0 && rx <= 8 && ry <= 8) {
+        if ( rx > 0 && ry > 0 && rx < 8 && ry <= 8) {
             coordinates.push([letters[rx], ry]);
         }
-
         rx = x + m[i][0]-1;
         ry = y + 2*m[i][1];
-        if (rx > 0 && ry > 0 && rx <= 8 && ry <= 8) {
+        if (rx > 0 && ry > 0 && rx < 8 && ry <= 8) {
             coordinates.push([letters[rx], ry]);
         }
     }
+
 
     let resultString = '';
 
